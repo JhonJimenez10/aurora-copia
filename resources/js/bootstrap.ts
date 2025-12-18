44c3aa85f8e41639f087591cc20d5e9a66114064
JhonJimenez12 <104083554+JhonJimenez12@users.noreply.github.com>
2025-12-18 08:49:16 -0500

    Modulo de traslados y confirmacion de traslado csfr token

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,18 +1,5 @@
 import axios from "axios";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-// ✅ Obtener el token CSRF del meta tag
-const token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    axios.defaults.headers.common["X-CSRF-TOKEN"] = (
-        token as HTMLMetaElement
-    ).content;
-} else {
-    console.error(
-        "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
-    );
-}
-
-export default axios;
